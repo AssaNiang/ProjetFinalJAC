fca93a9266b1a8352e32fac1265367958d32a289
JulieNonnon <julie.nonnon@gmail.com>
2023-01-03 15:51:04 +0100

    [FEATURE] création page All Categories
    création structure HTML, les liens pour navigation par les boutons doivent etre réalisés une fois le component de la page Catégorie Produit (modele) est pret

--- src/app/pages/all-categories/all-categories.component.ts
+++ src/app/pages/all-categories/all-categories.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
 
+
+interface allCategoriesBloc {
+  imageCategory : string;
+  descriptionCategory : string;
+  buttonCategory: string;
+  linkCategory: string;
+}
+
+
 @Component({
   selector: 'app-all-categories',
   templateUrl: './all-categories.component.html',
   styleUrls: ['./all-categories.component.css']
 })
+
+
+
 export class AllCategoriesComponent {
+
+allCategoryArray: allCategoriesBloc [] = [
+
+  {
+    imageCategory : "assets/Images/allcategories_tshirt.png",
+    descriptionCategory : "Jetez un oeil à notre gamme de produit",
+    buttonCategory: "T-Shirts",
+    linkCategory: "/category-skirts"
+  },
+
+  {
+    imageCategory : "assets/Images/allcategories_jackets.png",
+    descriptionCategory : "Jetez un oeil à notre gamme de produit",
+    buttonCategory: "Vestes",
+    linkCategory: "/category-skirts"
+  },
+
+  {
+    imageCategory : "assets/Images/allcategories_skirt.png",
+    descriptionCategory : "Jetez un oeil à notre gamme de produit",
+    buttonCategory: "Jupes",
+    linkCategory: "/category-skirts"
+  },
+
+  {
+    imageCategory : "assets/Images/allcategories_pants.png",
+    descriptionCategory : "Jetez un oeil à notre gamme de produit",
+    buttonCategory: "Pantalons",
+    linkCategory: "/category-skirts"
+  }
+
+]
+
+
 
 }
