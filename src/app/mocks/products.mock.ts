1c16ed568c1e1eb1cbc63ba051aa7704b7c64182
AssaNiang <mbayeou576@gmail.com>
2023-01-03 13:04:09 +0100

    création du mock ,creer un service ,j'ai fais les pages categories de pantalons / et un pantalon

---
+++ src/app/mocks/products.mock.ts
@@ -0,0 +1,74 @@
+type Category='skirts'|'pants';
+export interface Product{
+    id:number;
+    title:string;
+    description:string;
+    price:number;
+    color:string;
+    image:string;
+    isAvaible:boolean;
+    category:Category;
+}
+export const PRODUCTS:Product[]=[
+    {
+        id:1,
+        title:"Pantalon Noir",
+        description:"Large s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stand ",
+        price:15,
+        color:"noir",
+        image:"asset/img/pants/pantalon-Noir",
+        isAvaible:true,
+        category:"pants"
+    },
+    {
+        id:2,
+        title:"Pantalon rose",
+        description:"Large s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stand ",
+        price:15,
+        color:"rose",
+        image:"asset/img/pants/pantalon-Noir",
+        isAvaible:true,
+        category:"pants"
+    },
+    {
+        id:3,
+        title:"Pantalon cargo",
+        description:"Large s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stand ",
+        price:15,
+        color:"kaki",
+        image:"asset/img/pants/pantalon-Noir",
+        isAvaible:true,
+        category:"pants"
+    },
+    {
+        id:4,
+        title:"Pantalon palazzo",
+        description:"Large s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stand ",
+        price:15,
+        color:"vert",
+        image:"asset/img/pants/pantalon-Noir",
+        isAvaible:true,
+        category:"pants"
+    },
+    {
+        id:5,
+        title:"Pantalon jeans",
+        description:"Large s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stand ",
+        price:15,
+        color:"jeans bleu",
+        image:"asset/img/pants/pantalon-Noir",
+        isAvaible:true,
+        category:"pants"
+    },
+    {
+        id:6,
+        title:"Pantalon beige",
+        description:"Large s simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stand ",
+        price:15,
+        color:"beige",
+        image:"asset/img/pants/pantalon-Noir",
+        isAvaible:true,
+        category:"pants"
+    },
+
+]; 
--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from 'src/app/mocks/products.mock';
+import { ProductsService } from 'src/app/services/products/products.service';
 
 @Component({
   selector: 'app-product-details',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent {
-
+productP?:Product;
+//on créer une fonction pour récupérer les produits ET pour assigner notre âge 404 si un produit n’est pas trouvé
+constructor(
+  private productService:ProductsService,
+  private activatedRoute:ActivatedRoute,
+  private router:Router
+){}
+ngOnInit(){
+  this.initProduct();
+  console.log('product',this.productP);
+}
+initProduct(){
+  const id =Number(this.activatedRoute.snapshot.paramMap.get('id'));
+  const foundProduct=this.productService.getProduct(id);
+  if(foundProduct){
+    this.productP=foundProduct;
+  }else{
+    this.router.navigate(['/not-found']);
+  }
+}
 }
