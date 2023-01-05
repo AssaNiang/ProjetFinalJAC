cd87872220fd8331f59c5c76f0b943d8363c8753
AssaNiang <mbayeou576@gmail.com>
2023-01-05 11:37:33 +0100

    suppression des fichiers en trop dans les components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AllCategoriesComponent } from './pages/all-categories/all-categories.component';
-import { CategoryPantsComponent } from './pages/category-pants/category-pants.component';
-import { CategoryJacketsComponent } from './pages/category-jackets/category-jackets.component';
-import { CategorySkirtsComponent } from './pages/category-skirts/category-skirts.component';
-import { CategoryTshirtsComponent } from './pages/category-tshirts/category-tshirts.component';
 import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 import { BasketComponent } from './pages/basket/basket.component';
 import { PaymentComponent } from './pages/payment/payment.component';
@@ -25,10 +21,6 @@ import { CategoryProductComponent } from './pages/category-product/category-prod
     FooterComponent,
     HomeComponent,
     AllCategoriesComponent,
-    CategoryPantsComponent,
-    CategoryJacketsComponent,
-    CategorySkirtsComponent,
-    CategoryTshirtsComponent,
     ProductDetailsComponent,
     BasketComponent,
     PaymentComponent,
