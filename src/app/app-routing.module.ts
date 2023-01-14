18f8aa04f7ed739406c15376f238b43bd81d2249
Cynthia Poiny <cynthia.poiny@gmail.com>
2023-01-14 09:32:47 +0100

    creation de la page commande validée

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { BasketComponent } from './pages/basket/basket.component';
 import { CategoryProductsComponent } from './pages/category-products/category-products.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { PaymentSucessComponent } from './pages/payment-sucess/payment-sucess.component';
 import { PaymentComponent } from './pages/payment/payment.component';
 import { ProductDetailsComponent } from './pages/product-details/product-details.component';
 
@@ -40,7 +41,10 @@ const routes: Routes = [
   path:"payment",
   component: PaymentComponent
 },
-
+{
+  path:"payment-sucess",
+  component: PaymentSucessComponent
+},
 {
   path:"not-found",
   component: NotFoundComponent
